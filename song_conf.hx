256d024a2c92f15bfbb9eda9bfca4cdc9dcf2226
YoshiCrafter29 <yoshicrafter.mkpc@gmail.com>
2022-01-13 22:53:59 +0100

    new file:   config.json 	new file:   data/freeplaySonglist.json 	new file:   data/test/test.json 	new file:   images/menu-characters.png 	new file:   images/menu-characters.xml 	new file:   images/test-week.png 	new file:   song_conf.hx 	new file:   songs/test/Inst.mp3 	new file:   songs/test/Inst.ogg 	new file:   songs/test/Voices.mp3 	new file:   songs/test/Voices.ogg 	new file:   weeks.json

---
+++ song_conf.hx
@@ -0,0 +1,7 @@
+// AVAILABLE VARS : difficulty, song
+// CHECK DOC -> https://github.com/YoshiCrafter29/YoshiEngine/blob/main/documentation/mod/intro.md
+
+switch(song) {
+    case "test":
+        scripts = ["Friday Night Funkin':stages/default_stage"];
+}
